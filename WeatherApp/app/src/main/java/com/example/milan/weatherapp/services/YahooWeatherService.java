6d347cc90360d34c5b94bc280aa0c45821e98d60
milu20 <milu20@g-mail.hu>
2016-01-08 15:32:53 +0100

    URLConnection
    URL és API megadása

--- WeatherApp/app/src/main/java/com/example/milan/weatherapp/services/YahooWeatherService.java
+++ WeatherApp/app/src/main/java/com/example/milan/weatherapp/services/YahooWeatherService.java
@@ -1,7 +1,12 @@
 package com.example.milan.weatherapp.services;
 
 import android.graphics.AvoidXfermode;
+import android.net.Uri;
 import android.os.AsyncTask;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by Milan on 2016.01.06..
@@ -9,6 +14,7 @@ import android.os.AsyncTask;
 public class YahooWeatherService {
     private WeatherServiceCallback callback;
     private  String location;
+    private Exception error;
 
     public YahooWeatherService(WeatherServiceCallback callback) {
         this.callback = callback;
@@ -18,10 +24,22 @@ public class YahooWeatherService {
         return location;
     }
 
-    public void refreshWeather(String location){
+    public void refreshWeather(final String location){
         new AsyncTask<String, Void, String>() {
             @Override
-            protected String doInBackground(String... params) {
+            protected String doInBackground(String... strings) {
+
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\")", location);
+
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=&format=json", Uri.encode(YQL));
+
+                URLConnection connection = url.openConnection();
+                try {
+                    URL url = new URL(endpoint);
+                } catch (MalformedURLException e) {
+                    error = e;
+                    return null;
+                }
                 return null;
             }
 
